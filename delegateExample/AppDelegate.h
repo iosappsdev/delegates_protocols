bef034c5d7854f6e0d7df3a00e2261032cc39f1c
iOS Apps Development <info@iosappsdev.org>
2014-03-30 21:08:18 -0700

    Initial Commit

---
+++ delegateExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  delegateExample
+//
+//  Created by Ricardo Ruiz on 3/30/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ delegateExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  delegateExample
+//
+//  Created by Ricardo Ruiz on 3/30/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
